cab3d91baf5cbcfe0c707a0104098101069e7981
Eunji <129590633+bianbbc87@users.noreply.github.com>
2025-11-07 17:46:39 +0900

    Revert "feat: springboot server 실행 makefile 개발"

--- app/domain/src/main/java/com/tdd/domain/base/UseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.tdd.domain.base
-
-import kotlinx.coroutines.flow.Flow
-
-abstract class UseCase<REQUEST, RESPONSE> {
-    abstract suspend operator fun invoke(request: REQUEST): Flow<RESPONSE>
-}
--- app/feature/onboarding/src/main/java/com/tdd/onboarding/OnBoardingEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.tdd.onboarding
-
-import com.tdd.ui.base.Event
-
-sealed class OnBoardingEvent: Event {
-    data object GoToInterViewPage: OnBoardingEvent()
-}
