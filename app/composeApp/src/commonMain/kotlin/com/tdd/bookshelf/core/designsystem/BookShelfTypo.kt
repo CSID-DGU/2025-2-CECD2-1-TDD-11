ccc56d195c8beea7ee3cd29bbc4d79ea72009013
emjayMJkim <minjeong030420@gmail.com>
2025-11-15 17:14:52 +0900

    add: v2.0.0 design_system 재설정
    - color 설정
    - typo 추가 및 설정
    
    Related to: #44

--- app/composeApp/src/commonMain/kotlin/com/tdd/bookshelf/core/designsystem/BookShelfTypo.kt
+++ app/composeApp/src/commonMain/kotlin/com/tdd/bookshelf/core/designsystem/BookShelfTypo.kt
@@ -4,16 +4,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import bookshelf.composeapp.generated.resources.Res
-import bookshelf.composeapp.generated.resources.pretendard_black
-import bookshelf.composeapp.generated.resources.pretendard_bold
-import bookshelf.composeapp.generated.resources.pretendard_extrabold
-import bookshelf.composeapp.generated.resources.pretendard_extralight
-import bookshelf.composeapp.generated.resources.pretendard_light
+import bookshelf.composeapp.generated.resources.omyu_pretty
 import bookshelf.composeapp.generated.resources.pretendard_medium
-import bookshelf.composeapp.generated.resources.pretendard_regular
 import bookshelf.composeapp.generated.resources.pretendard_semibold
-import bookshelf.composeapp.generated.resources.pretendard_thin
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.Font
 
@@ -21,18 +16,178 @@ import org.jetbrains.compose.resources.Font
 @Composable
 fun setFont(): FontFamily =
     FontFamily(
-        Font(Res.font.pretendard_black, FontWeight.Black),
-        Font(Res.font.pretendard_bold, FontWeight.Bold),
-        Font(Res.font.pretendard_extrabold, FontWeight.ExtraBold),
-        Font(Res.font.pretendard_extralight, FontWeight.ExtraLight),
-        Font(Res.font.pretendard_light, FontWeight.Light),
-        Font(Res.font.pretendard_medium, FontWeight.Medium),
-        Font(Res.font.pretendard_regular, FontWeight.Normal),
         Font(Res.font.pretendard_semibold, FontWeight.SemiBold),
-        Font(Res.font.pretendard_thin, FontWeight.Thin),
+        Font(Res.font.pretendard_medium, FontWeight.Medium),
+        Font(Res.font.omyu_pretty, FontWeight.Normal),
     )
 
 object BookShelfTypo {
+    val Head1: TextStyle
+        @Composable get() =
+            TextStyle(
+                fontFamily = setFont(),
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 20.sp,
+                lineHeight = 56.sp
+            )
+
+    val Head2: TextStyle
+        @Composable get() =
+            TextStyle(
+                fontFamily = setFont(),
+                fontWeight = FontWeight.Medium,
+                fontSize = 20.sp,
+                lineHeight = 56.sp
+            )
+
+    val Head3: TextStyle
+        @Composable get() =
+            TextStyle(
+                fontFamily = setFont(),
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 18.sp,
+                lineHeight = 28.sp
+            )
+
+    val Head4: TextStyle
+        @Composable get() =
+            TextStyle(
+                fontFamily = setFont(),
+                fontWeight = FontWeight.Medium,
+                fontSize = 18.sp,
+                lineHeight = 28.sp
+            )
+
+    val Head10: TextStyle
+        @Composable get() =
+            TextStyle(
+                fontFamily = setFont(),
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 40.sp,
+            )
+
+    val Head20: TextStyle
+        @Composable get() =
+            TextStyle(
+                fontFamily = setFont(),
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 32.sp,
+            )
+
+    val Head30: TextStyle
+        @Composable get() =
+            TextStyle(
+                fontFamily = setFont(),
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 24.sp,
+            )
+
+    val Body1: TextStyle
+        @Composable get() =
+            TextStyle(
+                fontFamily = setFont(),
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 16.sp,
+                lineHeight = 24.sp
+            )
+
+    val Body2: TextStyle
+        @Composable get() =
+            TextStyle(
+                fontFamily = setFont(),
+                fontWeight = FontWeight.Medium,
+                fontSize = 16.sp,
+                lineHeight = 24.sp
+            )
+
+    val Body3: TextStyle
+        @Composable get() =
+            TextStyle(
+                fontFamily = setFont(),
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 14.sp,
+                lineHeight = 20.sp
+            )
+
+    val Body4: TextStyle
+        @Composable get() =
+            TextStyle(
+                fontFamily = setFont(),
+                fontWeight = FontWeight.Medium,
+                fontSize = 14.sp,
+                lineHeight = 20.sp
+            )
+
+    val Body10: TextStyle
+        @Composable get() =
+            TextStyle(
+                fontFamily = setFont(),
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 24.sp,
+            )
+
+    val Body20: TextStyle
+        @Composable get() =
+            TextStyle(
+                fontFamily = setFont(),
+                fontWeight = FontWeight.Medium,
+                fontSize = 24.sp,
+            )
+
+    val Body30: TextStyle
+        @Composable get() =
+            TextStyle(
+                fontFamily = setFont(),
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 18.sp,
+            )
+
+    val Body40: TextStyle
+        @Composable get() =
+            TextStyle(
+                fontFamily = setFont(),
+                fontWeight = FontWeight.Medium,
+                fontSize = 18.sp,
+            )
+
+    val Caption1: TextStyle
+        @Composable get() =
+            TextStyle(
+                fontFamily = setFont(),
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 12.sp,
+                lineHeight = 18.sp
+            )
+
+    val Caption2: TextStyle
+        @Composable get() =
+            TextStyle(
+                fontFamily = setFont(),
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 10.sp,
+                lineHeight = 12.sp
+            )
+
+    val Caption3: TextStyle
+        @Composable get() =
+            TextStyle(
+                fontFamily = setFont(),
+                fontWeight = FontWeight.Normal,
+                fontSize = 18.sp,
+                lineHeight = 28.sp
+            )
+
+    val Caption4: TextStyle
+        @Composable get() =
+            TextStyle(
+                fontFamily = setFont(),
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.sp,
+                lineHeight = 20.sp
+            )
+
+
+    // Legacy
     val Black: TextStyle
         @Composable get() =
             TextStyle(
--- app/composeApp/src/commonMain/kotlin/com/tdd/bookshelf/core/designsystem/Color.kt
+++ app/composeApp/src/commonMain/kotlin/com/tdd/bookshelf/core/designsystem/Color.kt
@@ -3,28 +3,39 @@ package com.tdd.bookshelf.core.designsystem
 import androidx.compose.ui.graphics.Color
 
 // BackGround
-val BackGround1 = Color(0xFFECEBF0)
-val BackGround2 = Color(0xFFF7F6FB)
-val BackGround3 = Color(0xFFF7F7F7)
-val BackGround4 = Color(0xFFF3F3F3)
+val BackGround1 = Color(0xFFF7F6FB)
+val BackGround2 = Color(0xFFFBF9F1)
+
+// Main
+val Main1 = Color(0xFF567AF3)
+val Main2 = Color(0xFF6ABCFE)
 
 // White
-val White0 = Color(0xFFFFFFFF)
-val White100 = Color(0x50E6E6E6)
-
-// Black
-val Black800 = Color(0xFF181D27)
-val Black900 = Color(0xFF000000)
-val Black1000 = Color(0x70000000)
-
-// Blue
-val Blue300 = Color(0xFF567AF3)
-val Blue400 = Color(0xFF2B4BF2)
-val Blue500 = Color(0xFF2A4ECA)
-val Blue700 = Color(0xFF0B4870)
-val Blue900 = Color(0xFF0601B4)
+val White1 = Color(0xFFFFFFFF)
+val White2 = Color(0xFFF5F9FE)
+val White3 = Color(0xFFF7F7F7)
+val White4 = Color(0xFFECEBF0)
 
 // Gray
+val Gray1 = Color(0xFFE9EBED)
+val Gray2 = Color(0xFFF1F4FA)
+val Gray3 = Color(0xFFD8D8D8)
+val Gray4 = Color(0xFF9FA4A9)
+val Gray5 = Color(0xFF838999)
+
+// Black
+val Black1 = Color(0xFF200E32)
+
+// Red
+val Red1 = Color(0xFFF76D6D)
+
+
+
+// Legacy
+val BackGround3 = Color(0xFFF7F7F7)
+val BackGround4 = Color(0xFFF3F3F3)
+val White0 = Color(0xFFFFFFFF)
+val White100 = Color(0x50E6E6E6)
 val Gray50 = Color(0xFFF5F9FE)
 val Gray100 = Color(0xFFE8E8E8)
 val Gray200 = Color(0xFFADADAD)
@@ -34,10 +45,14 @@ val Gray500 = Color(0xFF838999)
 val Gray600 = Color(0xFF7C8BA0)
 val Gray800 = Color(0xFF3B4054)
 val Gray900 = Color(0xFF262626)
-
-// Neutral
+val Black800 = Color(0xFF181D27)
+val Black900 = Color(0xFF000000)
+val Black1000 = Color(0x70000000)
+val Red500 = Color(0xFFDB2A12)
 val Neutral500 = Color(0xFF848FAC)
 val Neutral900 = Color(0xFF192252)
-
-// Red
-val Red500 = Color(0xFFDB2A12)
+val Blue300 = Color(0xFF567AF3)
+val Blue400 = Color(0xFF2B4BF2)
+val Blue500 = Color(0xFF2A4ECA)
+val Blue700 = Color(0xFF0B4870)
+val Blue900 = Color(0xFF0601B4)
