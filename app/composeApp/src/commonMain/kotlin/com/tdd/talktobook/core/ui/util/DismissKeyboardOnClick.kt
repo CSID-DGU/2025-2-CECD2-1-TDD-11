e78cdc8c164990a4ff9cc7b3d9c2ca747a0b8138
emjayMJkim <minjeong030420@gmail.com>
2025-12-03 17:48:45 +0900

    feat: 키보드 action 추가
    - textfield 입력 중 키보드 dismiss action 추가
    
    Related to: #63

---
+++ app/composeApp/src/commonMain/kotlin/com/tdd/talktobook/core/ui/util/DismissKeyboardOnClick.kt
@@ -0,0 +1,33 @@
+package com.tdd.talktobook.core.ui.util
+
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+
+@Composable
+fun DismissKeyboardOnClick(
+    content: @Composable () -> Unit,
+) {
+    val focusManager = LocalFocusManager.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onTap = {
+                        focusManager.clearFocus(force = true)
+                        keyboardController?.hide()
+                    },
+                )
+            },
+    ) {
+        content()
+    }
+}
